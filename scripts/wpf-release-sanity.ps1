ab3554362c1313928061b27eaf90471365f23cef
Cyber <cyber@users.noreply.github.com>
2026-06-01 22:56:08 +0100

    Polish custom match generator layout

--- scripts/wpf-release-sanity.ps1
+++ scripts/wpf-release-sanity.ps1
@@ -1,5 +1,5 @@
 param(
-    [string]$Version = "0.3.89-beta"
+    [string]$Version = "0.3.90-beta"
 )
 
 $ErrorActionPreference = "Stop"
--- windows-wpf/scripts/build-wpf-release.ps1
+++ windows-wpf/scripts/build-wpf-release.ps1
@@ -1,5 +1,5 @@
 param(
-    [string]$Version = "0.3.89-beta",
+    [string]$Version = "0.3.90-beta",
     [string]$Configuration = "Release",
     [string]$Runtime = "win-x64",
     [switch]$SelfContained,
